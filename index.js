df77357c8962e6c9e955f436345702e470ed493b
numahn <noman710@protonmail.com>
2022-11-06 13:45:07 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2')
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "password"
+})
+
+con.connect(function(err){
+  if (err) throw err;
+  console.log("connected")
+})
