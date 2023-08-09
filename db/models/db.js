2c600f39b5deeda514e723830b0a0b7e0f4e9afe
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-08-08 23:41:16 -0400

    removed hardcode

--- db/models/db.js
+++ db/models/db.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql2")
+require("dotenv").config()
 
 const dbConfig = {
-  host: "localhost",
-  user: "root",
-  password: "password",
-  db: "db"
+  // host: "localhost",
+  // user: "root",
+  // password: "password",
+  // db: "db
+  host: `${process.env.HOST}`,
+  user: `${process.env.USER}`,
+  password: `${process.env.PASSWORD}`,
+  db: `${process.env.DB}`
 }
 
 var connection = mysql.createPool({
